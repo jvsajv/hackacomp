52a002666ab0994cfdee3fb4c43b852d89ca6f3b
Joao Vitor dos Santos Almeida <runner.troll.rt@gmail.com>
2023-05-24 03:54:42 -0300

    refactor: Changes the navbar to a existing bootstrap component and make it responsive to mobile devices

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,8 +1,27 @@
 import React from "react";
-export default function Home(){
-    return(
-        <div>
-            <h1>Home</h1>
-        </div>
+import "../styles/Home.css"
+import { Parallax, Background } from "react-parallax";
+import background from "../assets/background_home.png"
+export default function Home() {
+    return (
+        <Parallax bgImage={background} strength={500}>
+            <div className="welcome-text">
+                <p>
+                    O nosso objetivo é promover uma maratona para que os alunos do dcomp
+                    criem soluções sustentáveis e regionais para os desafios globais (SPOILER ALERT:
+                    O TEMA TEM COMO BASE OS OBJETIVOS DE DESENVOLVIMENTO
+                    SUSTENTÁVEL DA ONU E REGIONALISMO).
+
+                </p>
+
+                <p>
+                    Os objetivos específicos incluem
+                    estimular a criatividade, desenvolver soluções inovadoras para problemas regionais
+                    específicos, incentivar a colaboração, fomentar o diálogo entre diferentes setores da
+                    sociedade e identificar oportunidades para a implementação das soluções
+                    desenvolvidas.
+                </p>
+            </div>
+        </Parallax>
     );
 }
--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,30 +1,50 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Navbar.css"
+import logo_hackacomp from "../assets/logo_hackacomp.png"
+import { Nav, Navbar, Offcanvas, Container } from "react-bootstrap";
+
 
 function NavbarItem(props) {
 
     if ("link" in props) {
         return (
-            <Link id={props.name.toLowerCase()} to={('/'+props.link)} className="navbarItem">
+            <Nav.Link eventKey={props.name.toLowerCase()} as={Link} to={('/' + props.link)} className="navbarItem">
                 {props.name}
-            </Link>);
+            </Nav.Link>
+        )
     }
     else {
         return (
-            <Link id={props.name.toLowerCase()} to={('/'+props.name)} className="navbarItem">
+            <Nav.Link eventKey={props.name.toLowerCase()} as={Link} to={('/' + props.name)} className="navbarItem">
                 {props.name}
-            </Link>
+            </Nav.Link>
         )
     }
 }
 
-export default function Navbar() {
+export default function NavigationBar() {
     return (
-        <div className="navbar">
-            <NavbarItem link="" name="Home" />
-            <NavbarItem name="Sobre" />
-            <NavbarItem name="Organizadores" />
-        </div>
+        <Navbar collapseOnSelect expand="md" className="navbar"  variant="dark">
+            <Container fluid className="shadow-sm m-0 p-0 rounded">
+                <Navbar.Brand><img src={logo_hackacomp} className="logo_hackacomp" /></Navbar.Brand>
+                <Navbar.Toggle aria-controls="navbarScroll" className="light"/>
+                <Navbar.Offcanvas id="navbarScroll" placement="end">
+                    <Offcanvas.Header closeButton>
+                        <Offcanvas.Title id={"navbarScroll"}>
+                            Menu
+                        </Offcanvas.Title>
+                    </Offcanvas.Header>
+                    <Offcanvas.Body>
+                        <Nav className="justify-content-start flex-grow-1 pe-3"
+                            navbarScroll>
+                            <NavbarItem name="Home" link="" />
+                            <NavbarItem name="Sobre" />
+                            <NavbarItem name="Organizadores" />
+                        </Nav>
+                    </Offcanvas.Body>
+                </Navbar.Offcanvas>
+            </Container>
+        </Navbar>
     );
 }
